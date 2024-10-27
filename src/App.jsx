f79189992e81a4aecf9aa89cea4f747c88fb5998
Edgar Migoyan <migoyane@gmail.com>
2024-10-27 20:02:39 +0400

    Change 27.10

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,27 @@ import "./App.css";
 
 
 function App() {
+  const [name,setName] = useState("");
+  const [age, setAge] = useState("")
+  const changeAge = (event)=>{
+    setAge(event.target.value)
+
+  }
+  const changeName = (event)=>{
+    setName(event.target.value)
+  }
+  const onSub = (event)=>{
+    event.preventDefault();
+    alert (`My name is ${name} ang i am ${age}`)
+  }
     return <>
   <Header />
   <div><h1>Hello world!</h1></div>
- 
+  <form action="" onSubmit={onSub}>
+    <input type="text" value={name} onChange={changeName} /> <br />
+    <input type="text" value={age} onChange={changeAge} />
+    <button type="submit">Click</button>
+  </form>
   </>;
 }
 
