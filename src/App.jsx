545eef0580acf370a73ae2bb1e497bbb0cb3e2fb
Edgar Migoyan <migoyane@gmail.com>
2024-10-29 19:59:21 +0400

    UseEffect day 29.10 50/50

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,43 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./Header";
 import "./App.css";
 
-
-
 function App() {
-  const [name,setName] = useState("");
-  const [age, setAge] = useState("")
-  const changeAge = (event)=>{
-    setAge(event.target.value)
-
-  }
-  const changeName = (event)=>{
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [second,setSecond] = useState(0)
+  const handleChangeName = (event)=>{
     setName(event.target.value)
   }
-  const onSub = (event)=>{
-    event.preventDefault();
-    alert (`My name is ${name} ang i am ${age}`)
+  const handleChangeAge = (event)=>{
+    setAge(event.target.value)
   }
-    return <>
-  <Header />
-  <div><h1>Hello world!</h1></div>
-  <form action="" onSubmit={onSub}>
-    <input type="text" value={name} onChange={changeName} /> <br />
-    <input type="text" value={age} onChange={changeAge} />
-    <button type="submit">Click</button>
-  </form>
-  </>;
+  const onSubmitted  =(event)=>{
+    event.preventDefault()
+    alert(`${name} ${age}`)
+  }
+ 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSecond(prevSecond => prevSecond + 1); // Используем стрелочную функцию правильно
+    }, 1000);
+  
+    return () => {
+      clearInterval(interval); // Используем 'interval' для очистки
+    };
+  }, []);
+  return (
+    <>
+      <h1>Hello my friends</h1>
+      <h2>Please chek this form </h2>
+      <form action="" onSubmit={onSubmitted}>
+        <input type="text" onChange={handleChangeName}/> <br />
+        <input type="text" onChange={handleChangeAge} /> <br />
+        <button type="submit">Submit</button>
+      </form>
+      <p>Second: {second}</p>
+    </>
+  );
 }
 
 export default App;
