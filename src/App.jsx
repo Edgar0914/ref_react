42af1ff51269822f7df9d4b9583c6d19054ea28d
Edgar Migoyan <migoyane@gmail.com>
2024-10-30 19:41:35 +0400

    To do-s add

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,22 @@ import Header from "./Header";
 import "./App.css";
 
 function App() {
-  const [name, setName] = useState("");
-  const [age, setAge] = useState("");
-  const [second,setSecond] = useState(0)
-  const handleChangeName = (event)=>{
-    setName(event.target.value)
+  const [action, setAction] = useState("")
+  const [doing, setDoing] = useState([])
+  const changeIn = (event)=>{
+    setAction(event.target.value)
   }
-  const handleChangeAge = (event)=>{
-    setAge(event.target.value)
-  }
-  const onSubmitted  =(event)=>{
+  const setSub = (event)=>{
     event.preventDefault()
-    alert(`${name} ${age}`)
+    setDoing([...doing,action])
+    setAction("")
   }
- 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setSecond(prevSecond => prevSecond + 1); // Используем стрелочную функцию правильно
-    }, 1000);
-  
-    return () => {
-      clearInterval(interval); // Используем 'interval' для очистки
-    };
-  }, []);
-  return (
-    <>
-      <h1>Hello my friends</h1>
-      <h2>Please chek this form </h2>
-      <form action="" onSubmit={onSubmitted}>
-        <input type="text" onChange={handleChangeName}/> <br />
-        <input type="text" onChange={handleChangeAge} /> <br />
-        <button type="submit">Submit</button>
-      </form>
-      <p>Second: {second}</p>
+  return(  <>
+  <form action="" onSubmit={setSub}>
+    <input type="text" placeholder="Enter your action" onChange={changeIn}/>
+    <button>Enter</button>
+  </form>
+  <h1>{doing.join("")}</h1>
     </>
   );
 }
